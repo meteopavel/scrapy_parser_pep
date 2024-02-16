773566781c31b978a81548aa36594e02c9deac3e
meteopavel <isu@meteopavel.ru>
2024-02-16 16:15:51 +0800

    code for review #1

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,13 +1,29 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import csv
+from collections import defaultdict
+from datetime import datetime
 
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+BASE_DIR = 'results'
 
 
 class PepParsePipeline:
+    def open_spider(self, spider):
+        self.status_counts = defaultdict(int)
+        self.timestamp = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
     def process_item(self, item, spider):
+        status = item.get('status')
+        self.status_counts[status] += 1
         return item
+
+    def close_spider(self, spider):
+        filename = f'{BASE_DIR}/status_summary_{self.timestamp}.csv'
+        with open(filename, 'w', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ('Статус', 'Количество')
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            total_pep_documents = sum(self.status_counts.values())
+            for status, count in self.status_counts.items():
+                writer.writerow({'Статус': status, 'Количество': count})
+            writer.writerow(
+                {'Статус': 'Total', 'Количество': total_pep_documents}
+            )
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,28 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        pass
+        numerical_index_section = response.css('section#numerical-index')
+        pep_links = numerical_index_section.css('a::attr(href)').extract()
+        pep_links = list(set(pep_links[1:]))
+        pep_links.sort()
+        for pep_link in pep_links:
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        number = response.css('.breadcrumbs li:nth-child(3)::text').get()
+        name = response.css('h1.page-title::text').get()
+        status = response.css('dd abbr::text').get()
+        data = {
+            'number': int(number.replace('PEP ', '')),
+            'name': name[name.find('–') + 2:],
+            'status': status,
+        }
+        yield PepParseItem(data)
